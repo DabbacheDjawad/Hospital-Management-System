884e9d356678aa75d8b969e3fc69746070308a48
DabbacheDjawad <djawadmne@gmail.com>
2025-03-04 17:12:54 +0100

    some css finalizations

--- Client/Components/Header.jsx
+++ Client/Components/Header.jsx
@@ -11,8 +11,8 @@ const Header = () => {
   return (
     <div className="flex justify-center w-full items-center mt-8 transition-all md:flex-col-reverse sm:flex-col-reverse lg:flex-row xl:flex-row">
   <Navbar
-    className={`flex flex-col ml-10 xl:min-h-fit lg:min-h-fit overflow-hidden transition-[max-height,opacity] duration-500 ease-in-out 
-    ${isOpen ? 'max-h-[500px] opacity-100 w-full' : 'max-h-0 opacity-0'}`}
+    className={`flex lg:w-auto xl:w-auto flex-col ml-10 xl:min-h-fit lg:min-h-fit overflow-hidden transition-[max-height,opacity] duration-500 ease-in-out 
+    ${isOpen ? 'max-h-[500px] opacity-100 w-full' : 'max-h-0 w-full opacity-0'}`}
   /> 
         <div className="flex justify-center items-center ">
             <Button href={"/api/v1/appointments"}>Sign In</Button>
